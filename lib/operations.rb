147adff3c4b6d05496ff524a09783cbd28d79262
Jennifer Dobak <jdobak1@gmail.com>
2018-01-09 19:31:25 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,12 @@
 def unsafe?(speed)
 	if speed > 60 || speed < 40
 		return true
+	else speed < 60 || speed > 40
+		return false
 end
 
 
 
 def not_safe?(speed)
-
+speed > 60 || speed < 40 ? true :
 end
